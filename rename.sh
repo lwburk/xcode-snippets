c7fe834a37e5ae1f8da9ec358419b8c6d7e6a776
Wayne Burkett <wayne.burkett@gmail.com>
2015-11-05 16:33:33 -0800

    Improve comments in rename script

--- rename.sh
+++ rename.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+# renames all snippets to a filename based on their title
+# requires XMLStarlet
+
 suffix="codesnippet"
 for snippet in `ls *.$suffix`;
 do
-    # requires XMLStarlet
     filename=$(xml sel --net -t -v "/plist/dict[1]/key[.='IDECodeSnippetTitle']/following-sibling::string[1]" $snippet | tr -cd '[[:alnum:]]').$suffix
     if [ "$snippet" != "$filename" ]; then
         echo "Renaming $snippet to $filename"
