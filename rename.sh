2eb32970a6e60764ef3ffb78066804430c697e7f
Wayne Burkett <wayne.burkett@gmail.com>
2015-11-05 16:38:31 -0800

    Split command over newline for readability

--- rename.sh
+++ rename.sh
@@ -6,7 +6,8 @@
 suffix="codesnippet"
 for snippet in `ls *.$suffix`;
 do
-    filename=$(xml sel --net -t -v "/plist/dict[1]/key[.='IDECodeSnippetTitle']/following-sibling::string[1]" $snippet | tr -cd '[[:alnum:]]').$suffix
+    filename=$(xml sel --net -t -v "/plist/dict[1]/key[.='IDECodeSnippetTitle']/following-sibling::string[1]" $snippet \
+        | tr -cd '[[:alnum:]]').$suffix
     if [ "$snippet" != "$filename" ]; then
         echo "Renaming $snippet to $filename"
         mv $snippet $filename
