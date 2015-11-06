8c0232cdbc7bf58134ab1c41378893d774600945
Wayne Burkett <wayne.burkett@gmail.com>
2015-11-05 16:32:23 -0800

    Comment rename script

--- rename.sh
+++ rename.sh
@@ -3,6 +3,7 @@
 suffix="codesnippet"
 for snippet in `ls *.$suffix`;
 do
+    # requires XMLStarlet
     filename=$(xml sel --net -t -v "/plist/dict[1]/key[.='IDECodeSnippetTitle']/following-sibling::string[1]" $snippet | tr -cd '[[:alnum:]]').$suffix
     if [ "$snippet" != "$filename" ]; then
         echo "Renaming $snippet to $filename"
